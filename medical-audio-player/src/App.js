c0d4323320231ce0030fdc408eeef3d2174d22f2
Swattle1 <samwattle99@gmail.com>
2024-06-01 02:22:16 +0100

    Create visual structure
    Created home page and visual structure of the app

--- medical-audio-player/src/App.js
+++ medical-audio-player/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Consultations from './pages/Consultations';
+import Settings from './pages/Settings';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div style={{ paddingTop: '56px' }}> {/* Adjust padding to account for fixed header */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/consultations" element={<Consultations />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
