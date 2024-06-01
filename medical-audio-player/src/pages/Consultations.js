618103f51ad1734ae74bdd400e0c3b50dfaca777
Swattle1 <samwattle99@gmail.com>
2024-06-01 16:25:36 +0100

    Title on Consultations

--- medical-audio-player/src/pages/Consultations.js
+++ medical-audio-player/src/pages/Consultations.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Consultations = () => {
-  return <div>Consultations Page</div>;
-};
-
+  return (
+    <div className="consultations-page">
+      <h1 className="text-center">Consultations</h1>
+    </div>
+  );
+}
 export default Consultations;
